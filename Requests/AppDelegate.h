a3c0d792afd4c027995b2e9c9089a623905384d5
shaohua yang <shaohua0110@gmail.com>
2018-05-11 11:30:16 +0800

    Initial Commit

---
+++ Requests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Requests
+//
+//  Created by shaohua on 2018/5/11.
+//  Copyright © 2018 syang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Requests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Requests
+//
+//  Created by shaohua on 2018/5/11.
+//  Copyright © 2018 syang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
