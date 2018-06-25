acee00e30da99266beabe59366e0568185da28f2
shaohua yang <shaohua0110@gmail.com>
2018-06-25 11:27:10 +0800

    transformResponse 无法输出 error

--- Requests/AppConfig.m
+++ Requests/AppConfig.m
@@ -58,31 +58,25 @@
                 }];
             }
             return [[RACSignal return:event] dematerialize];
-        }] map:^id (Query *value) {
-            if (value.listKey) { // 支持分页需要原始的 Query
-                NSArray *original = value.responseObject;
-                NSMutableArray *result = [NSMutableArray arrayWithArray:original]; // 解除循环引用
-                [result addObject:value];
-                return result;
-            };
-            return value.responseObject; // 绝大部分调用只关心解析后的结果
+        }] flattenMap:^RACSignal *(Query *value) {
+            NSArray *x = value.responseObject;
+            // 全局解析
+            if (value.modelClass) {
+                NSArray *cursor = x[0];
+                NSArray *items = x[1];
+
+                NSError *error = nil;
+                NSArray *objects = [MTLJSONAdapter modelsOfClass:value.modelClass fromJSONArray:items error:&error];
+                if (error) {
+                    return [RACSignal error:error];
+                }
+                return [RACSignal return:@[cursor, objects, value]]; // 支持分页需要原始的 Query；注意循环引用
+            }
+            return [RACSignal return:x]; // 绝大部分调用只关心解析后的结果
         }];
     };
 
     manager.transformResponse = ^id (Query *query, id responseObject) {
-        // 全局解析
-        if (query.modelClass) {
-            NSArray *cursor = responseObject[0];
-            NSArray *items = responseObject[1];
-
-            NSError *error = nil;
-            NSArray *objects = [MTLJSONAdapter modelsOfClass:query.modelClass fromJSONArray:items error:&error];
-            if (error) {
-                return [RACSignal error:error];
-            }
-            responseObject = @[cursor, objects];
-        }
-
         // 全局拼接
         if (!query.listKey || !query.responseObject) {
             return responseObject;
@@ -91,8 +85,10 @@
         id last = query.responseObject;
         NSDictionary *cursor = responseObject[0];
         if ([last[0][@"page"] intValue] + 1 == [cursor[@"page"] intValue]) {
-            [last[1] addObjectsFromArray:responseObject[1]];
-            return @[cursor, last[1]];
+            NSMutableArray *concat = [NSMutableArray array];
+            [concat addObjectsFromArray:last[1]];
+            [concat addObjectsFromArray:responseObject[1]];
+            return @[cursor, concat];
         }
         return responseObject;
     };
