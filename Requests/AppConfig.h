4984e0990527138e2f5336702b35dc522039d27c
syang <syang@local>
2019-01-03 18:00:29 +0800

    +

--- Requests/AppConfig.h
+++ Requests/AppConfig.h
@@ -9,6 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <AFNetworking/AFNetworking.h>
 
+/*
+ 参考实现
+
+ 根据使用的 API，支持
+    全局的登录重试
+    JSON 转 Native
+    分页 API 的合并等
+
+ 其它功能如全局的 URL 改写等
+ */
 @interface AppConfig : NSObject
 
 + (AFHTTPSessionManager *)manager;
